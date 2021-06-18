cebf3168dd94742fdd7ce0548a1e3ce47620e676
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2021-06-18 23:07:23 +0300

    Added RobotCliRunner

--- src/main/java/il/ac/bgu/cs/bp/bprobot/RobotCliRunner.java
+++ src/main/java/il/ac/bgu/cs/bp/bprobot/RobotCliRunner.java
@@ -1,49 +1,212 @@
 package il.ac.bgu.cs.bp.bprobot;
 
+import il.ac.bgu.cs.bp.bpjs.analysis.*;
+import il.ac.bgu.cs.bp.bpjs.analysis.listeners.PrintDfsVerifierListener;
+import il.ac.bgu.cs.bp.bpjs.analysis.violations.Violation;
 import il.ac.bgu.cs.bp.bpjs.execution.BProgramRunner;
+import il.ac.bgu.cs.bp.bpjs.execution.listeners.PrintBProgramRunnerListener;
 import il.ac.bgu.cs.bp.bpjs.model.BProgram;
 import il.ac.bgu.cs.bp.bpjs.model.ResourceBProgram;
+import il.ac.bgu.cs.bp.bpjs.model.eventselection.EventSelectionStrategy;
+import il.ac.bgu.cs.bp.bpjs.model.eventselection.LoggingEventSelectionStrategyDecorator;
+import il.ac.bgu.cs.bp.bpjs.model.eventselection.SimpleEventSelectionStrategy;
 import il.ac.bgu.cs.bp.bprobot.util.communication.CommunicationHandler;
 import il.ac.bgu.cs.bp.bprobot.util.communication.ICommunication;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.EvaluatorException;
+import org.mozilla.javascript.Scriptable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class RobotCliRunner {
 
   public static void main(String[] args) throws IOException, TimeoutException {
-    if (!List.of(0, 1, 3, 4).contains(args.length)) {
+    if (args.length == 0) {
       printUsageAndExit();
     }
 
-    String filename = args.length == 1 || args.length == 4 ? args[0] : "SampleRobot.js";
-    BProgram bProgram = new ResourceBProgram(filename);
-    bProgram.setWaitForExternalEvents(true);
-    BProgramRunner rnr = new BProgramRunner(bProgram);
-
-    // Print program events to the console
-//        rnr.addListener( new PrintBProgramRunnerListener() );
-    ICommunication communication = new CommunicationHandler();
-    if (args.length == 3) {
-      communication.setCredentials(args[0], args[1], args[2]);
-    } else if (args.length == 4) {
-      communication.setCredentials(args[1], args[2], args[3]);
-    }
-    rnr.addListener(new RobotBProgramRunnerListener(communication, bProgram));
-
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      try {
-        communication.closeConnection();
-        System.out.println("Connection Closed!");
-      } catch (IOException | TimeoutException e) {
-        e.printStackTrace();
+    BProgram bpp = new BProgram("BPjs") {
+      @Override
+      protected void setupProgramScope(Scriptable scope) {
+        for (String arg : args) {
+          if (arg.equals("-")) {
+            println(" [READ] stdin");
+            try {
+              evaluate(System.in, "stdin", Context.getCurrentContext());
+            } catch (EvaluatorException ee) {
+              logScriptExceptionAndQuit(ee, arg);
+            }
+          } else {
+            if (!arg.startsWith("-")) {
+              Path inFile = Paths.get(arg);
+              println(" [READ] %s", inFile.toAbsolutePath().toString());
+              if (!Files.exists(inFile)) {
+                println("File %s does not exit", inFile.toAbsolutePath().toString());
+                System.exit(-2);
+              }
+              try (InputStream in = Files.newInputStream(inFile)) {
+                evaluate(in, arg, Context.getCurrentContext());
+              } catch (EvaluatorException ee) {
+                logScriptExceptionAndQuit(ee, arg);
+              } catch (IOException ex) {
+                println("Exception while processing " + arg + ": " + ex.getMessage());
+                Logger.getLogger(RobotCliRunner.class.getName()).log(Level.SEVERE, null, ex);
+              }
+            }
+          }
+          println(" [ OK ] %s", arg);
+        }
       }
-    }));
-    // go!
-    rnr.run();
+
+      private void logScriptExceptionAndQuit(EvaluatorException ee, String arg) {
+        println("Error in source %s:", arg);
+        println(ee.details());
+        println("line: " + ee.lineNumber() + ":" + ee.columnNumber());
+        println("source: " + ee.lineSource());
+        System.exit(-3);
+      }
+    };
+
+    bpp.setWaitForExternalEvents(true);
+    SimpleEventSelectionStrategy sess = new SimpleEventSelectionStrategy();
+    if (switchPresent("--verify", args)) {
+      bpp.setEventSelectionStrategy(sess);
+      DfsBProgramVerifier vfr = new DfsBProgramVerifier();
+      vfr.setDebugMode(switchPresent("-v", args));
+      vfr.setProgressListener(new PrintDfsVerifierListener());
+
+      if (switchPresent("--full-state-storage", args)) {
+        println("Using full state storage");
+        vfr.setVisitedStateStore(new BThreadSnapshotVisitedStateStore());
+      } else {
+        vfr.setVisitedStateStore(new BProgramSnapshotVisitedStateStore());
+      }
+      if (switchPresent("--liveness", args)) {
+        vfr.addInspection(ExecutionTraceInspections.HOT_SYSTEM);
+        vfr.addInspection(ExecutionTraceInspections.HOT_BTHREADS);
+        vfr.addInspection(ExecutionTraceInspections.HOT_TERMINATIONS);
+      }
+      String maxDepthStr = keyForValue("--max-trace-length", args);
+      if (maxDepthStr != null) {
+        try {
+          long maxDepth = Long.parseLong(maxDepthStr.trim());
+          vfr.setMaxTraceLength(maxDepth);
+        } catch (NumberFormatException nfe) {
+          println("Illegal max trace length value: '" + maxDepthStr + "'.");
+          System.exit(-5);
+        }
+      }
+      println("Max trace length: " + vfr.getMaxTraceLength());
+
+      if (vfr.getInspections().isEmpty()) {
+        ExecutionTraceInspections.DEFAULT_SET.forEach(vfr::addInspection);
+      }
+      println("Inspections:");
+      vfr.getInspections().forEach(ins -> println(" * " + ins.title()));
+
+      try {
+        println("Starting verification");
+        VerificationResult res = vfr.verify(bpp);
+        println("Verification completed.");
+
+        if (res.getViolation().isPresent()) {
+          Violation vio = res.getViolation().get();
+          println("Found Violation:");
+          println(vio.decsribe());
+
+          println("Counter example trace:");
+          vio.getCounterExampleTrace().getNodes().stream()
+              .map(n -> n.getEvent())
+              .filter(Optional::isPresent)
+              .map(Optional::get)
+              .forEach(evt -> println(evt.toString()));
+
+        } else {
+          println("No violations found");
+        }
+
+        println("General statistics:");
+        println(String.format("Time:\t%,d (msec)", res.getTimeMillies()));
+        println(String.format("States scanned:\t%,d", res.getScannedStatesCount()));
+        println(String.format("Edges scanned:\t%,d", res.getScannedEdgesCount()));
+
+
+      } catch (Exception e) {
+        println("!! Exception during verifying the program: " + e.getMessage());
+        println("!! Stack trace:");
+        e.printStackTrace(System.out);
+      }
+
+    } else {
+      EventSelectionStrategy ess = switchPresent("-v", args) ? new LoggingEventSelectionStrategyDecorator(sess) : sess;
+      bpp.setEventSelectionStrategy(ess);
+      BProgramRunner bpr = new BProgramRunner(bpp);
+
+      ICommunication communication = new CommunicationHandler();
+      String credentials = keyForValue("credentials", args);
+      if (credentials != null) {
+        String[] cred = credentials.split("[,]");
+        if (cred.length != 3) {
+          printUsageAndExit();
+        }
+        communication.setCredentials(cred[0], cred[1], cred[2]);
+      }
+      bpr.addListener(new RobotBProgramRunnerListener(communication, bpp));
+
+      Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        try {
+          communication.closeConnection();
+          System.out.println("Connection Closed!");
+        } catch (IOException | TimeoutException e) {
+          e.printStackTrace();
+        }
+      }));
+
+      if (!switchPresent("-v", args)) {
+        bpr.addListener(new PrintBProgramRunnerListener());
+      }
+
+      bpr.run();
+    }
+  }
+
+  /**
+   * @return {@code true} iff the passed switch is present in args.
+   */
+  private static boolean switchPresent(String aSwitch, String[] args) {
+    return Arrays.stream(args).anyMatch(s -> s.trim().equals(aSwitch));
+  }
+
+  private static String keyForValue(String aKey, String[] args) {
+    for (String arg : args) {
+      if (arg.startsWith(aKey + "=")) {
+        String[] comps = arg.split("=", 2);
+        return comps.length == 2 ? comps[1] : null;
+      }
+    }
+    return null;
+  }
+
+  private static void println(String template, String... params) {
+    print(template + "\n", params);
+  }
+
+  private static void print(String template, String... params) {
+    if (params.length == 0) {
+      System.out.print(template);
+    } else {
+      System.out.printf(template, (Object[]) params);
+    }
   }
 
   private static void printUsageAndExit() {
